37f2b877958bb24eebc2f040b291f90e7398021e
Sumit <sumitswadesh000@gmail.com>
2022-10-28 17:57:42 +0530

    update

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -24,16 +24,20 @@ const Blog = ({data}) => {
             </ul>
           </div>
         </div>
-        <Row>
+        <div className='bloginfo-main'>
+        <Row gutter={[16, 16]}>
           {data.map((item) => (
             <Col className="gutter-row" xs={24} lg={8} md={12}
              href={`./blog/${item.id}`} key={item.id}>
+             <div className='bloginfo'>
                <h4>{item.id}</h4>
                 <p>{item.title}</p>
+                </div>
             </Col>
          )
          )}
         </Row>
+        </div>
         <Footer/>
         </Layout>
     </div>
